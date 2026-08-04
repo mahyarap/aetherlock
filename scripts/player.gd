b02d749a1b68b45f32907f263dce2a0e8258f40f
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2026-08-04 22:38:19 +0200

    Lesson 3

---
+++ scripts/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export_range(50.0, 600.0, 10.0) var move_speed: float = 260.0
+
+func _physics_process(delta: float) -> void:
+	var input_direction: Vector2 = Input.get_vector(
+			"move_left",
+			"move_right",
+			"move_up",
+			"move_down",
+	)
+	velocity = input_direction * move_speed
+	move_and_slide()
