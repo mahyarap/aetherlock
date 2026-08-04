48798f6cfd06a28fbab66a2531f284b6a6dd36e2
arianbig2 <arianbig2@gmail.com>
2026-08-04 10:27:48 -0700

    Added label 2

--- scripts/bootstrap.gd
+++ scripts/bootstrap.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @onready var status_label: Label = $StatusLabel
+@onready var controls_label: Label = $ControlsLabel
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	status_label.text = "Aetherlock labratory online"
+	controls_label.text = "Move ..."
 	print("Bootstrap scene ready")
