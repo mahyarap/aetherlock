7603be8b3ab7209ec99eadf899d1f75ff535304d
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2026-08-05 17:42:11 +0200

    Lesson 4

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export_range(50.0, 600.0, 10.0) var move_speed: float = 260.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var input_direction: Vector2 = Input.get_vector(
 			"move_left",
 			"move_right",
