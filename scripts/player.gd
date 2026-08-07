0ccf65de8964da43c9a012fff8e6f0ce66b25d60
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2026-08-07 20:12:20 +0200

    Lesson 6

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,25 @@
 extends CharacterBody2D
 
+const PROJECTILE_SCENE: PackedScene = preload(
+	"res://scenes/combat/projectile.tscn"
+)
+
 @export_range(50.0, 600.0, 10.0) var move_speed: float = 260.0
+@export_range(0.05, 1.0, 0.05) var fire_cooldown: float = 0.25
 
 @onready var aim_pivot: Node2D = $AimPivot
+@onready var muzzle: Marker2D = $AimPivot/Muzzle
+@onready var attack_timer: Timer = $AttackCooldown
 
 var aim_direction: Vector2 = Vector2.RIGHT
 
-func _process(delta: float) -> void:
-	var new_aim_direction: Vector2 = global_position.direction_to(
-		get_global_mouse_position()
-	)
-	if new_aim_direction != Vector2.ZERO:
-		aim_direction = new_aim_direction
-		aim_pivot.rotation = aim_direction.angle()
+func _ready() -> void:
+	attack_timer.wait_time = fire_cooldown
+
+func _process(_delta: float) -> void:
+	_update_aim()
+	if Input.is_action_pressed("attack") and attack_timer.is_stopped():
+		_fire_projectile()
 
 func _physics_process(_delta: float) -> void:
 	var input_direction: Vector2 = Input.get_vector(
@@ -23,3 +30,20 @@ func _physics_process(_delta: float) -> void:
 	)
 	velocity = input_direction * move_speed
 	move_and_slide()
+
+func _update_aim() -> void:
+	var new_aim_direction: Vector2 = global_position.direction_to(
+		get_global_mouse_position()
+	)
+	if new_aim_direction != Vector2.ZERO:
+		aim_direction = new_aim_direction
+		aim_pivot.rotation = aim_direction.angle()
+		
+func _fire_projectile() -> void:
+	var projectile: Projectile = (
+			PROJECTILE_SCENE.instantiate() as Projectile
+	)
+
+	get_tree().current_scene.add_child(projectile)
+	projectile.initialize(muzzle.global_position, aim_direction)
+	attack_timer.start()
---
+++ scripts/projectile.gd
@@ -0,0 +1,27 @@
+class_name Projectile
+extends Area2D
+
+@export_range(100.0, 1200.0, 25.0) var speed: float = 650.0
+
+var direction: Vector2 = Vector2.RIGHT
+
+func initialize(
+	spawn_position: Vector2,
+	travel_direction: Vector2,
+) -> void:
+	global_position = spawn_position
+	direction = travel_direction.normalized()
+	rotation = direction.angle()
+	
+func _physics_process(delta: float) -> void:
+	global_position += direction * speed * delta
+	
+func _on_body_entered(_body: Node2D) -> void:
+	queue_free()
+	
+func on_lifetime_timeout() -> void:
+	queue_free()
+
+
+func _on_lifetime_timeout() -> void:
+	pass # Replace with function body.
